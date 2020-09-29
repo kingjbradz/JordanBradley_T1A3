0bcebc0d7b0631f57696207fc0bdd1fb588c4ebb
Jordan Bradley <jordanbradley@Jordans-MacBook-Pro.local>
2020-09-29 16:56:09 +1000

    added colorize, colorized most of the outputs for user friendliness

--- src/main.rb
+++ src/main.rb
@@ -1,126 +1,120 @@
-require 'csv'
-require 'json'
-require 'tabulo'
-require 'colorize'
+require "csv"
+require "json"
+require "tabulo"
+require "colorize"
 
 chores = []
 
-
-def read
-        puts "Which would you like to read,"
-        puts "Chore Types or Chore History?"
-        answer = gets.chomp.downcase
-    if answer == "chore types" or answer == "chore type"
-        CSV.foreach(("chore-list.csv"), headers: true) do |line|
-            puts "#{line['number']}: #{line['chore']}"
-        end
-    elsif answer == "chore history"
-        File.open("chore-history.json", mode: "r") do |line|
-            p "#{line}"
-        end
-    end
-end
-
-
-
 def create
-    puts "What new Chore would you like to create?"
-    new_chore = gets.chomp.downcase
-    puts "#{new_chore}"
-    puts "Save '#{new_chore}' to Chore List?"
-    answer = gets.chomp
-    if answer == "yes" or answer == "y"
-    File.open("chore-list.csv", "a") 
+  puts "What new Chore would you like to create?".blue
+  new_chore = gets.chomp.downcase
+  puts "#{new_chore}"
+  puts "Save '#{new_chore}' to Chore List?"
+  answer = gets.chomp
+  if answer == "yes" or answer == "y"
+    File.open("chore-list.csv", "a")
     #user input
-    #output input 
+    #output input
     #ask user if OK
-    #if no, go to output message 
+    #if no, go to output message
     #if yes, save to CSV chore list file
-    end
+  end
 end
 
 def edit
-    puts "Which Chore would you like to edit?"
-    CSV.foreach(("chore-list.csv"), headers: true) do |line|
-        puts "#{line['number']}: #{line['chore']}"
-        chores.push line.to_h
-    end
-    answer = gets.chomp.downcase
-    #output chore list
-    #user input
-    #check validility - if invalid, output error message
-    #ask user what input would like to change to
-    #input new name 
-    #output input
-    #ask user if OK
-    #if no, go to output message 
-    #if yes, save to CSV chore list file
+  puts "Which Chore would you like to edit?".blue
+  CSV.foreach(("chore-list.csv"), headers: true) do |line|
+    puts "#{line["Number"]}: #{line["Chore"]}"
+    chores.push line.to_h
+  end
+  answer = gets.chomp.downcase
+  #output chore list
+  #user input
+  #check validility - if invalid, output error message
+  #ask user what input would like to change to
+  #input new name
+  #output input
+  #ask user if OK
+  #if no, go to output message
+  #if yes, save to CSV chore list file
 end
 
 def delete
-    puts "Which Chore would you like to delete?"
-    CSV.foreach(("chore-list.csv"), headers: true) do |line|
-        puts "#{line['number']}: #{line['chore']}"
-        chores.push line.to_h
-    end
-    answer = gets.chomp.downcase
-    #user input
-    #check validility - if invalid, output error message
-    #output input 
-    #ask user if OK
-    #if no, go to output message 
-    #if yes, write to CSV chore list file
+  puts "Which Chore would you like to delete?".blue
+  CSV.foreach(("chore-list.csv"), headers: true) do |line|
+    puts "#{line["Number"]}: #{line["Chore"]}"
+    chores.push line.to_h
+  end
+  answer = gets.chomp.downcase
+  #user input
+  #check validility - if invalid, output error message
+  #output input
+  #ask user if OK
+  #if no, go to output message
+  #if yes, write to CSV chore list file
 end
 
 loop do
-    
-    puts "Welcome to Home Task"
-    puts "Please type and enter the feature you wish to use:"
-    puts "Do Chore | Read | Create | Edit | Delete | Exit"
-    menu_select = gets.chomp.downcase
+  puts "Welcome to Home Task!".red
+  puts "Please type and enter the feature you wish to use:".light_red
+  puts "Do Chore | Read | Create | Edit | Delete | Exit".cyan
+  menu_select = gets.chomp.downcase
 
-    case menu_select
+  case menu_select
 
-    when "do", "do chore", "dochore", "chore"
-        puts "Which Chore would you like to do?"
-        CSV.foreach(("chore-list.csv"), headers: true, ) do |line|
-            puts "#{line['number']}: #{line['chore']}"
-            chores.push line.to_h
-        end
-        answer = gets.chomp
-        the_chore = chores.find { |chore| chore['number'] == answer } 
-        puts "What is your name?"
-        name = gets.chomp
-        puts "#{the_chore["chore"]} done by #{name}. Is this correct?"
-        confirm = gets.chomp
-        if confirm == 'yes'
-            File.write("chore-history.csv", "\n#{the_chore["chore"]}, #{name}, #{Time.now}",  mode: "a")          
-        end
-        #check validility - if invalid, output error message
-        #ask user if OK
-        #if no, redo  
-
-    when "read"
-        read
-    
-    when "create"
-        create
-
-    when "edit"
-        edit
-
-    when "delete"
-        delete
-        
-    when "exit"
-        puts "Exiting application, are you sure? If yes, type 'yes'."
-        answer = gets.chomp.downcase
+  when "do", "do chore", "dochore", "chore"
+    puts "Which Chore would you like to do?".blue
+    CSV.foreach(("chore-list.csv"), headers: true) do |line|
+      puts "#{line["Number"]}: #{line["Chore"]}".green
+      chores.push line.to_h
+    end
+    answer = gets.chomp
+    the_chore = chores.find { |chore| chore["Number"] == answer }
+    # if value is not valid
+    #  output "Invalid! Try again."
+    # return to start of section
+    # end
+    puts "What is your name?".blue
+    name = gets.chomp
+    puts "#{the_chore["Chore"]} done by #{name}. Is this correct?".blue
+    confirm = gets.chomp
+    if confirm == "yes"
+      File.write("chore-history.csv", "\n#{the_chore["Chore"]}, #{name}, #{Time.now}", mode: "a")
+      puts "Chore is logged. Thanks #{name}!".blue
+    # else
+    #  if value answer is not yes
+    # return to start of loop
+    end
+  when "read"
+    puts "Which would you like to read,".blue
+    puts "Chore Types or Chore History?".light_blue
+    answer = gets.chomp.downcase
+    if answer == "chore types" or answer == "chore type"
+      CSV.foreach(("chore-list.csv"), headers: true) do |line|
+        puts "#{line["Number"]}: #{line["Chore"]}".green
+      end
+    elsif answer == "chore history"
+      CSV.foreach("chore-history.csv", headers: true) do |line|
+        puts "#{line["Chore"]} done by#{line["Name"]} at#{line["Date"]}.".green
+      end
+    end
+  when "create"
+    create
+  when "edit"
+    edit
+  when "delete"
+    delete
+  when "exit"
+    puts "Exiting application, are you sure?".blue
+    puts "If yes, type 'yes'.".light_blue
+    answer = gets.chomp.downcase
     if answer == "yes" or answer == "y"
-        puts "Closing application. Thank you!"
-        exit
-    else  
-        puts "Returning to title screen."
+      puts "Closing application. Thank you!".yellow
+      exit
+    else
+      puts "Returning to title screen.".magenta
     end
-    
-    end
+  else
+    puts "Invalid input"
+  end
 end
