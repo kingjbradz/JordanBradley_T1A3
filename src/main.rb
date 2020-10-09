c388dc363376c08ad1d2704a6a91bfd67a9cdc68
Jordan Bradley <jordanbradley@Jordans-MBP.lan>
2020-10-09 10:56:57 +1000

    added methods ruby file

--- src/main.rb
+++ src/main.rb
@@ -5,18 +5,7 @@ require 'tty-table'
 require 'smarter_csv'
 require 'colorize'
 
-def output_capture(question)
-  puts question
-  input = gets.chomp
-  return input
-end
-
-def exit_application
-  leaving_font = TTY::Font.new(:standard)
-  puts leaving_font.write('GOODBYE!', letter_spacing: 2).yellow
-  puts 'Closing application. Thank you!'.red
-  exit
-end
+require_relative 'methods.rb'
 
 chores = []
 
